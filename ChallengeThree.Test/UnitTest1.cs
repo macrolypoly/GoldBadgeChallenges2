242465f84101b2878bddc55e0f2ec4fb594d8362
macrolypoly <krodri1150@gmail.com>
2021-03-30 19:24:42 -0400

    unittest challenge three

--- ChallengeThree.Test/UnitTest1.cs
+++ ChallengeThree.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using ChallengeThree.Repository;
 
 namespace ChallengeThree.Test
 {
@@ -7,8 +9,18 @@ namespace ChallengeThree.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1_AddBadge()
         {
+            Badge badge = new Badge();
+            BadgeRepository repo = new BadgeRepository();
+            Dictionary<int, List<string>> kvp = repo.ReturnBadge();
+
+            int beforeCount = kvp.Count;
+            repo.AddBadgeToDict(badge);
+            int actual = kvp.Count;
+            int expected = beforeCount + 1;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
